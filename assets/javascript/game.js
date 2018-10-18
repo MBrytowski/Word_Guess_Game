ee47b4fa2feeaf01fd39a2e2df442a777ae0bd93
MNBrytowski <mn.brytowski@gmail.com>
2018-10-18 15:26:14 -0600

    update

---
+++ assets/javascript/game.js
@@ -0,0 +1,23 @@
+var game = {
+    "wins": 0,
+    "guesses": 9,
+    "words": [
+        "monopoly",
+        "sorry",
+        "chutes and ladders",
+        "candyland",
+        "mastermind",
+        "risk",
+        "clue"
+    ]
+
+    
+
+};
+
+
+document.onkeyup = function (event) {
+    var letter = event.key.toLowerCase();
+
+
+};
